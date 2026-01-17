346605ff7321963030632e9f833c327c0203da73
MarcosAugusto47 <MarcosAugusto47@users.noreply.github.com>
2026-01-17 15:24:19 -0300

    Add GitHub Actions workflow for GitHub Pages deployment

--- publishconf.py
+++ publishconf.py
@@ -5,7 +5,7 @@ sys.path.append(os.curdir)
 from pelicanconf import *
 
 # If your site is available via HTTPS, make sure SITEURL begins with https://
-SITEURL = "https://marcosaugusto.com"  # Update with your domain
+SITEURL = "https://marcosaugusto47.github.io/website"
 RELATIVE_URLS = False
 FEED_ALL_ATOM = "feed.xml"
 DELETE_OUTPUT_DIRECTORY = True
